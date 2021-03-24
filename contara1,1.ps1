1fd9846b36815b5ce5a3465316719ddd24523df0
F31 <81227546+F31-code@users.noreply.github.com>
2021-03-24 12:17:57 +0100

    Create contara1,1.ps1

---
+++ contara1,1.ps1
@@ -0,0 +1,17 @@
+$loopCount = 0
+do{
+    $answer = Read-Host "Input a number 1 - 10" 
+
+    Write-Host "You entered $answer"
+
+    $loopCount++
+
+    if($answer -eq 4){
+        $answered = $true
+    }
+
+
+
+} until ($answered -eq $true)
+
+Write-Host ("Looped {0} times" -f $loopCount)
